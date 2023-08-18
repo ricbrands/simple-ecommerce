1f69e034a489a2028614707b66c7d924c0d1912d
Ricardo Brandao <rpvbrandao@gmail.com>
2023-08-17 23:24:10 -0300

    feat(route): added route to the app, created cart, pageNotFound and detail components

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import './App.css';
+import Footer from "./Footer";
+import Header from "./Header";
+import Products from './Products';
+import { Routes, Route } from "react-router-dom";
+import Detail from "./Detail";
+import Cart from "./Cart";
+
+function App() {
+  return (
+    <>
+      <div className="content">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<h1>Welcome to a simple ecommerce page!</h1>} />
+            <Route path="/:category" element={<Products />} />
+            <Route path="/:category/:id" element={<Detail />} />
+            <Route path="/cart" element={<Cart />} />            
+          </Routes>
+        </main>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
