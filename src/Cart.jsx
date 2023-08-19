a6dc47c66bee5d3fab9d81b349ab53e00f2a7c58
Ricardo Brandao <rpvbrandao@gmail.com>
2023-08-18 23:26:35 -0300

    feat(checkout): created checkout component and added validations on the fields

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import useFetchAll from "./services/useFetchAll";
 import Spinner from "./Spinner";
 
 export default function Cart({ cart, updateQuantity}) {
+    const navigate = useNavigate();
     const urls = cart.map((i) => `products/${i.id}`);
     const { data: products, loading, error } = useFetchAll(urls);
 
@@ -50,6 +52,11 @@ export default function Cart({ cart, updateQuantity}) {
                 ? "Your cart is empty" : 
                 `${numItemsInCart} Item${numItemsInCart > 1 ? "s" : ""} in my Cart`}</h1>
             <ul>{cart.map(renderItem)}</ul>
+            { cart.length > 0 && (
+                <button className="btn btn-primary" onClick={() => navigate("/checkout")}>
+                    Checkout
+                </button>
+            )}
         </section>
     )
 }
