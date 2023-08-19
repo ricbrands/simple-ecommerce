8002f324b29cfc762cf95e2e328321c83021e81c
Ricardo Brandao <rpvbrandao@gmail.com>
2023-08-18 23:42:53 -0300

    feat(useFetch): improve the fetch to check the urls dependency on useEffect

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,29 +1,35 @@
-import { useState, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 export default function useFetch(url) {
+    const isMountedRef = useRef(false);
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        isMountedRef.current = true;
         async function init() {
             try {
                 const response = await fetch(baseUrl + url);
                 if (response.ok) {
                     const json = await response.json();
-                    setData(json);
+                    if (isMountedRef.current) setData(json);
                 } else {
                     throw response;
                 }
             } catch (e) {
-                setError(e);
+                if (isMountedRef.current) setError(e);
             } finally {
-                setLoading(false);
+                if(isMountedRef.current) setLoading(false);
             }
         }
         init();
+
+        return () => {
+            isMountedRef.current = false;
+        }
     }, [url]);
 
     return { data, error, loading };
--- src/services/useFetchAll.js
+++ src/services/useFetchAll.js
@@ -1,11 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function useFetchAll(urls) {
+    const prevUrls = useRef([]);
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        // Only run if the array of URLs passed in changes
+        if (areEqual(prevUrls.current, urls)) {
+            setLoading(false);
+            return;
+        }
+        prevUrls.current = urls;
+
         const promises = urls.map((url) => 
             fetch(process.env.REACT_APP_API_BASE_URL + url).then((response) => {
                 if (response.ok) return response.json();
@@ -20,7 +28,15 @@ export default function useFetchAll(urls) {
                 setError(e);
             })
             .finally(() => setLoading(false));
-    }, []);
+    }, [urls]);
 
     return { data, loading, error };
+}
+
+// keeping the function outside the hook avoids the function being re-allocated on each render 
+function areEqual(array1, array2) {
+    return (
+        array1.length === array2.length &&
+        array1.every((value, index) => value === array2[index])
+    );
 }
