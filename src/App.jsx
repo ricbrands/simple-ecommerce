705ba69522bafba3e1e6cb3eb619f1ab709ec671
Ricardo Brandao <rpvbrandao@gmail.com>
2023-08-18 21:29:23 -0300

    feat(cart): implemented cart page, addToCart and updateQuantity functions in App, passed by props to cart and detail components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
 import Footer from "./Footer";
 import Header from "./Header";
@@ -8,6 +8,30 @@ import Detail from "./Detail";
 import Cart from "./Cart";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(id, sku) {
+    setCart( (items) => {
+      const itemInCart = items.find((i) => i.sku === sku);
+      if (itemInCart) {
+        return items.map( (i) => 
+          i.sku === sku ? { ...i, quantity: i.quantity + 1} : i
+        );
+      } else {
+        return [...items, { id, sku, quantity: 1}];
+      }
+    })
+  }
+
+  function updateQuantity(sku, quantity) {
+    setCart((items) => {
+      if (quantity === 0) {
+        return items.filter((i) => i.sku !== sku); 
+      }
+      return items.map((i) => i.sku=== sku ? {...i, quantity}: i);
+    });
+  }
+
   return (
     <>
       <div className="content">
@@ -16,8 +40,8 @@ function App() {
           <Routes>
             <Route path="/" element={<h1>Welcome to a simple ecommerce page!</h1>} />
             <Route path="/:category" element={<Products />} />
-            <Route path="/:category/:id" element={<Detail />} />
-            <Route path="/cart" element={<Cart />} />            
+            <Route path="/:category/:id" element={<Detail addToCart={addToCart}/>} />
+            <Route path="/cart" element={<Cart cart={cart} updateQuantity={updateQuantity}/>} />            
           </Routes>
         </main>
       </div>
---
+++ src/services/useFetchAll.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+export default function useFetchAll(urls) {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const promises = urls.map((url) => 
+            fetch(process.env.REACT_APP_API_BASE_URL + url).then((response) => {
+                if (response.ok) return response.json();
+                throw response;
+            })
+        );
+
+        Promise.all(promises)
+            .then((json) => setData(json))
+            .catch((e) => {
+                console.error(e);
+                setError(e);
+            })
+            .finally(() => setLoading(false));
+    }, []);
+
+    return { data, loading, error };
+}
