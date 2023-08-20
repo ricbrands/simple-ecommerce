eddb3ac5e687cf218849718413c1af5bdd9c9a66
Ricardo Brandao <rpvbrandao@gmail.com>
2023-08-19 23:51:52 -0300

    feat(cartContext): refactored app to use context to share the functions and data of cart. Created the cartContext and a custom useContext hook to made it easy to consume the context.

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -3,8 +3,10 @@ import { useParams, useNavigate } from "react-router-dom";
 import useFetch from './services/useFetch';
 import Spinner from './Spinner';
 import PageNotFound from './PageNotFound';
+import { useCart } from './cartContext';
 
-export default function Detail(props) {
+export default function Detail() {
+    const { dispatch } = useCart();
     const { id } = useParams();
     const navigate = useNavigate();
     const [sku, setSku] = useState("");
@@ -31,7 +33,7 @@ export default function Detail(props) {
                     disabled={!sku}
                     className="btn btn-primary" 
                     onClick={() => {
-                        props.dispatch({type: "add", id, sku });
+                        dispatch({type: "add", id, sku });
                         navigate("/cart");
                         }
                     }
