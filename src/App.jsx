7cc36fcb3adfaa53a7fe00ddd6b0d80bf7af7f0c
Ricardo Brandao <rpvbrandao@gmail.com>
2023-08-19 23:24:41 -0300

    feat(reducer): switched from useState to useReducer. Created the cartReducer to handle the state of cart.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 import './App.css';
 import Footer from "./Footer";
 import Header from "./Header";
@@ -7,44 +7,20 @@ import { Routes, Route } from "react-router-dom";
 import Detail from "./Detail";
 import Cart from "./Cart";
 import Checkout from "./Checkout";
+import cartReducer from "./cartReducer";
+
+let initialCart;
+try {
+  initialCart = JSON.parse(localStorage.getItem("cart")) ?? [];
+} catch {
+  console.error("The cart could not be parsed into JSON.");
+  initialCart = [];
+}
 
 function App() {
-  const [cart, setCart] = useState(() => {
-    try {
-      return JSON.parse(localStorage.getItem("cart")) ?? [];
-    } catch {
-      console.error("The cart could not be parsed into JSON.");
-      return [];
-    }
-  });
+  const [cart, dispatch] = useReducer(cartReducer, initialCart);
 
   useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
-
-  function addToCart(id, sku) {
-    setCart( (items) => {
-      const itemInCart = items.find((i) => i.sku === sku);
-      if (itemInCart) {
-        return items.map( (i) => 
-          i.sku === sku ? { ...i, quantity: i.quantity + 1} : i
-        );
-      } else {
-        return [...items, { id, sku, quantity: 1}];
-      }
-    })
-  }
-
-  function updateQuantity(sku, quantity) {
-    setCart((items) => {
-      if (quantity === 0) {
-        return items.filter((i) => i.sku !== sku); 
-      }
-      return items.map((i) => i.sku=== sku ? {...i, quantity}: i);
-    });
-  }
-
-  function emptyCart() {
-    setCart([]);
-  }
 
   return (
     <>
@@ -54,9 +30,9 @@ function App() {
           <Routes>
             <Route path="/" element={<h1>Welcome to a simple ecommerce page!</h1>} />
             <Route path="/:category" element={<Products />} />
-            <Route path="/:category/:id" element={<Detail addToCart={addToCart}/>} />
-            <Route path="/cart" element={<Cart cart={cart} updateQuantity={updateQuantity}/>} />   
-            <Route path="/checkout" element={<Checkout cart={cart} emptyCart={emptyCart}/>} />         
+            <Route path="/:category/:id" element={<Detail dispatch={dispatch}/>} />
+            <Route path="/cart" element={<Cart cart={cart} dispatch={dispatch}/>} />   
+            <Route path="/checkout" element={<Checkout cart={cart} dispatch={dispatch}/>} />         
           </Routes>
         </main>
       </div>
--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -14,7 +14,7 @@ const emptyAddress = {
     country: "",
 }
 
-export default function Checkout({ cart, emptyCart }) {
+export default function Checkout({ cart, dispatch }) {
     const [address, setAddress] = useState(emptyAddress);
     const [status, setStatus] = useState(STATUS.IDLE);
     const [saveError, setSaveError] = useState(null);
@@ -22,7 +22,7 @@ export default function Checkout({ cart, emptyCart }) {
 
     // derived state
     const errors = getErrors(address);
-    const isValid = Object.keys.length === 0;
+    const isValid = Object.keys(errors).length === 0;
 
     function handleChange(e) {
         e.persist(); // persist the event (isn't necessary in React 17 or newer since React 17 no longer pools events)
@@ -46,7 +46,7 @@ export default function Checkout({ cart, emptyCart }) {
         if (isValid) {
             try {
                 await saveShippingAddress(address);
-                emptyCart();
+                dispatch({ type: "empty" });
                 setStatus(STATUS.COMPLETED);
             } catch (e) {
                 setSaveError(e);
